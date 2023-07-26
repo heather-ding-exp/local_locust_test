cf80035b52b85d17c2da68f987e166fe430a389c
hding <t-hding@expediagroup.com>
2023-07-26 11:34:48 -0700

    fix

--- locustfile.py
+++ locustfile.py
@@ -15,25 +15,24 @@ class FeastUser(HttpUser):
         print(response.json())
 
 # class FeastUser(HttpUser):
-    
-    @task
-    def push(self):
-        event_dict = {
-            "driver_id": [1001],
-            "event_timestamp": [str(datetime(2021, 5, 13, 10, 59, 42))],
-            "created": [str(datetime(2021, 5, 13, 10, 59, 42))],
-            "conv_rate": [1.0],
-            "acc_rate": [3.0],
-            "avg_daily_trips": [1000],
-            "string_feature": "test2",
-        }
-        push_data = {
-            "push_source_name":"driver_stats_push_source",
-            "df":event_dict,
-            "to":"online",
-        }
-        self.client.post(
-            "/push",
-            data=json.dumps(push_data))
+#     @task
+#     def push(self):
+#         event_dict = {
+#             "driver_id": [1001],
+#             "event_timestamp": [str(datetime(2021, 5, 13, 10, 59, 42))],
+#             "created": [str(datetime(2021, 5, 13, 10, 59, 42))],
+#             "conv_rate": [1.0],
+#             "acc_rate": [3.0],
+#             "avg_daily_trips": [1000],
+#             "string_feature": "test2",
+#         }
+#         push_data = {
+#             "push_source_name":"driver_stats_push_source",
+#             "df":event_dict,
+#             "to":"online",
+#         }
+#         self.client.post(
+#             "/push",
+#             data=json.dumps(push_data))
         
     
